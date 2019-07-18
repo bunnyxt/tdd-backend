2fe954ce5315b988fc9dea6d5c7a423795971a59
bunnyxt <bunnyxt@outlook.com>
2019-07-18 14:30:49 -0700

    Add require file for batch require.

---
+++ tdd2/model/require.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once("Member.php");
+require_once("SprintDaily.php");
+require_once("SprintDailyRecord.php");
+require_once("SprintVideo.php");
+require_once("SprintVideoRecord.php");
+
+?>
---
+++ tdd2/require.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once("db/require.php");
+require_once("model/require.php");
+require_once("util/require.php");
+
+?>
