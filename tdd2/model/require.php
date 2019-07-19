2416256983818a8a5383524bbcd24d565ba5a83c
bunnyxt <bunnyxt@outlook.com>
2019-07-18 23:06:36 -0700

    Remove batch require files.

--- tdd2/model/require.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once("Member.php");
-require_once("SprintDaily.php");
-require_once("SprintDailyRecord.php");
-require_once("SprintVideo.php");
-require_once("SprintVideoRecord.php");
-
-?>
--- tdd2/require.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once("db/require.php");
-require_once("model/require.php");
-require_once("util/require.php");
-
-?>
