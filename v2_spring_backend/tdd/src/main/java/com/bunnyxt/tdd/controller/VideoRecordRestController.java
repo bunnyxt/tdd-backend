67fcb7026bb1031a95308ae5ba368cdaedbe4b74
bunnyxt <bunnyxt@outlook.com>
2019-11-16 19:40:53 +0800

    Add @CrossOrigin.

--- v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/controller/VideoRecordRestController.java
+++ v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/controller/VideoRecordRestController.java
@@ -3,13 +3,11 @@ package com.bunnyxt.tdd.controller;
 import com.bunnyxt.tdd.model.VideoRecord;
 import com.bunnyxt.tdd.service.VideoRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class VideoRecordRestController {
 
