863953ce97132a8fb0f758dd3d1d160170295676
bunnyxt <bunnyxt@outlook.com>
2019-11-18 22:51:53 +0800

    Add fragment class.

---
+++ v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/model/fragment/MemberFragment.java
@@ -0,0 +1,45 @@
+package com.bunnyxt.tdd.model.fragment;
+
+public class MemberFragment {
+
+    private String sex;
+    private String name;
+    private String face;
+    private String sign;
+
+    public MemberFragment() {
+
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFace() {
+        return face;
+    }
+
+    public void setFace(String face) {
+        this.face = face;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+}
---
+++ v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/model/fragment/VideoRecordFragment.java
@@ -0,0 +1,79 @@
+package com.bunnyxt.tdd.model.fragment;
+
+public class VideoRecordFragment {
+
+    private int added;
+    private int view;
+    private int danmaku;
+    private int reply;
+    private int favorite;
+    private int coin;
+    private int share;
+    private int like;
+
+
+    public int getAdded() {
+        return added;
+    }
+
+    public void setAdded(int added) {
+        this.added = added;
+    }
+
+    public int getView() {
+        return view;
+    }
+
+    public void setView(int view) {
+        this.view = view;
+    }
+
+    public int getDanmaku() {
+        return danmaku;
+    }
+
+    public void setDanmaku(int danmaku) {
+        this.danmaku = danmaku;
+    }
+
+    public int getReply() {
+        return reply;
+    }
+
+    public void setReply(int reply) {
+        this.reply = reply;
+    }
+
+    public int getFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(int favorite) {
+        this.favorite = favorite;
+    }
+
+    public int getCoin() {
+        return coin;
+    }
+
+    public void setCoin(int coin) {
+        this.coin = coin;
+    }
+
+    public int getShare() {
+        return share;
+    }
+
+    public void setShare(int share) {
+        this.share = share;
+    }
+
+    public int getLike() {
+        return like;
+    }
+
+    public void setLike(int like) {
+        this.like = like;
+    }
+
+}
