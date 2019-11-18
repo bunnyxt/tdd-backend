8aba300b476ed65ff82c2f33f61fbc08911d1fe3
bunnyxt <bunnyxt@outlook.com>
2019-11-18 22:46:01 +0800

    Remove demo apis like queryVideosByTid and queryVideosVc.

--- v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/controller/VideoRestController.java
+++ v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/controller/VideoRestController.java
@@ -5,7 +5,6 @@ import com.bunnyxt.tdd.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -13,24 +12,5 @@ public class VideoRestController {
 
     @Autowired
     private VideoService videoService;
-
-    @RequestMapping(value = "/video/aid/{aid}", method = RequestMethod.GET)
-    public Video queryVideoByAid(@PathVariable int aid) {
-        return videoService.queryVideoByAid(aid);
-    }
-
-    @RequestMapping(value = "/videos/tid/{tid}", method = RequestMethod.GET)
-    public List<Video> queryVideosByTid(@PathVariable int tid,
-                                        @RequestParam(defaultValue = "1") int pn,
-                                        @RequestParam(defaultValue = "50") int ps) {
-        return videoService.queryVideosByTid(tid, pn, ps);
-    }
-
-    @RequestMapping(value = "/videos/vc", method = RequestMethod.GET)
-    public List<Video> queryVideosVc(@RequestParam(defaultValue = "1") int pn,
-                                     @RequestParam(defaultValue = "50") int ps) {
-        return videoService.queryVideosVc(pn, ps);
-    }
-
 
 }
