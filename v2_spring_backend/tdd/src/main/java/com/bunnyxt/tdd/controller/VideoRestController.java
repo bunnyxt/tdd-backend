e0fd486eab0afb397d091ca444d7266d06030921
bunnyxt <bunnyxt@outlook.com>
2020-01-18 12:08:41 +0800

    Add PUT /video/{aid} to update video.

--- v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/controller/VideoRestController.java
+++ v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/controller/VideoRestController.java
@@ -1,5 +1,6 @@
 package com.bunnyxt.tdd.controller;
 
+import com.bunnyxt.tdd.error.InvalidRequestParameterException;
 import com.bunnyxt.tdd.model.Video;
 import com.bunnyxt.tdd.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,5 +13,22 @@ public class VideoRestController {
 
     @Autowired
     private VideoService videoService;
+
+    @RequestMapping(value = "/video/{aid}", method = RequestMethod.PUT)
+    public Video updateVideoByAid(@PathVariable int aid, @RequestBody Video video)
+            throws InvalidRequestParameterException {
+        // when use PUT method, you must provide all parameters of video, since all params will be updated
+        // if you only want to change one param, use PATCH method instead
+
+        // check params
+        if (aid != video.getAid()) {
+            throw new InvalidRequestParameterException("aid", aid,
+                    "aid should be same as video.aid provided in request body, which is " + video.getAid());
+        }
+
+        videoService.updateVideoByAid(aid, video);
+
+        return videoService.queryVideoByAid(aid);
+    }
 
 }
--- v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/service/VideoService.java
+++ v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/service/VideoService.java
@@ -4,4 +4,8 @@ import com.bunnyxt.tdd.model.Video;
 
 public interface VideoService {
 
+    void updateVideoByAid(int aid, Video video);
+
+    Video queryVideoByAid(int aid);
+
 }
