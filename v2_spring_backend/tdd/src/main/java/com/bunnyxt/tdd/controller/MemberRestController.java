4ef6449758c4f9caf574110a844a31b04d0d5687
bunnyxt <bunnyxt@outlook.com>
2020-02-15 18:46:38 +0800

    Remove member entrance from Member Controller.

--- v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/controller/MemberRestController.java
+++ v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/controller/MemberRestController.java
@@ -19,45 +19,4 @@ public class MemberRestController {
     @Autowired
     private MemberService memberService;
 
-    @RequestMapping(value = "/member/{mid}", method = RequestMethod.GET)
-    public Member queryMemberByMid(@PathVariable Integer mid) {
-        return memberService.queryMemberByMid(mid);
-    }
-
-    @RequestMapping(value = "/member", method = RequestMethod.GET)
-    public ResponseEntity<List<Member>> queryMembers(@RequestParam(defaultValue = "") String sex,
-                                                     @RequestParam(defaultValue = "") String name,
-                                                     @RequestParam(defaultValue = "1") Integer pn,
-                                                     @RequestParam(defaultValue = "20") Integer ps)
-            throws InvalidRequestParameterException {
-        // check params
-        List<String> allowedSex = new ArrayList<>();
-        allowedSex.add("男");
-        allowedSex.add("女");
-        allowedSex.add("保密");
-        if (!sex.equals("")) {
-            if (!allowedSex.contains(sex)) {
-                throw new InvalidRequestParameterException("sex", sex, "sex should in " + allowedSex.toString());
-            }
-        }
-        if (pn <= 0) {
-            throw new InvalidRequestParameterException("pn", pn, "pn should be greater than 0");
-        }
-        if (ps <= 0 || ps > 20) {
-            throw new InvalidRequestParameterException("ps", ps, "ps should between 1 and 20");
-        }
-
-        // get list
-        List<Member> list = memberService.queryMembers(sex, name, pn, ps);
-
-        // get total count
-        Integer totalCount = memberService.queryMembersCount(sex, name);
-
-        // add headers
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("x-total-count", String.valueOf(totalCount));
-        headers.add("Access-Control-Allow-Headers", "x-total-count");
-        headers.add("Access-Control-Expose-Headers", "x-total-count");
-        return new ResponseEntity<>(list, headers, HttpStatus.OK);
-    }
 }
