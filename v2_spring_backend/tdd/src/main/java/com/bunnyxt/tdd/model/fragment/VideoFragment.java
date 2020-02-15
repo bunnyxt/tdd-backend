e6161ebf34be70ad0970ec63fa2c34a86d593fd2
bunnyxt <bunnyxt@outlook.com>
2020-02-15 18:45:08 +0800

    Create VideoFragment.java

---
+++ v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/model/fragment/VideoFragment.java
@@ -0,0 +1,60 @@
+package com.bunnyxt.tdd.model.fragment;
+
+public class VideoFragment {
+
+    private Integer aid;
+    private String pic;
+    private String title;
+    private Integer pubdate;
+    private Integer mid;
+    private VideoRecordFragment laststat;
+
+
+    public Integer getAid() {
+        return aid;
+    }
+
+    public void setAid(Integer aid) {
+        this.aid = aid;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getPubdate() {
+        return pubdate;
+    }
+
+    public void setPubdate(Integer pubdate) {
+        this.pubdate = pubdate;
+    }
+
+    public Integer getMid() {
+        return mid;
+    }
+
+    public void setMid(Integer mid) {
+        this.mid = mid;
+    }
+
+    public VideoRecordFragment getLaststat() {
+        return laststat;
+    }
+
+    public void setLaststat(VideoRecordFragment laststat) {
+        this.laststat = laststat;
+    }
+}
