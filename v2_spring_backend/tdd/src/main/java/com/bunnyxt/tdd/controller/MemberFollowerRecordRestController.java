9a215621194c112f19ee681351299f9a81c8f4c8
bunnyxt <bunnyxt@outlook.com>
2020-02-19 16:07:48 +0800

    Rename add Rest.

--- v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/controller/MemberFollowerRecordRestController.java
+++ v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/controller/MemberFollowerRecordRestController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @CrossOrigin
 @RestController
-public class MemberFollowerRecordController {
+public class MemberFollowerRecordRestController {
 
     @Autowired
     MemberFollowerRecordService memberFollowerRecordService;
