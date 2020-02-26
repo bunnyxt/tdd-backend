a8535a38fd63270cae7efda8fadf583253556852
bunnyxt <bunnyxt@outlook.com>
2020-02-26 18:22:58 +0800

    Add more params check.

--- v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/util/TddParamCheckUtil.java
+++ v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/util/TddParamCheckUtil.java
@@ -4,6 +4,40 @@ import com.bunnyxt.tdd.error.InvalidRequestParameterException;
 
 public class TddParamCheckUtil {
 
+    private static void _zero_plus(Integer value, String name) {
+        if (value < 0) {
+            throw new InvalidRequestParameterException(name, value, name + " should be greater than 0");
+        }
+    }
+
+    private static void _zero_plus(Long value, String name) {
+        if (value < 0) {
+            throw new InvalidRequestParameterException(name, value, name + " should be greater than 0");
+        }
+    }
+
+    public static void userid(Long userid){
+        // default: 0, no limit
+        _zero_plus(userid, "userid");
+    }
+
+    public static void start_ts(Integer start_ts){
+        // default: 0, no limit
+        _zero_plus(start_ts, "start_ts");
+    }
+
+    public static void end_ts(Integer end_ts){
+        // default: 0, no limit
+        _zero_plus(end_ts, "end_ts");
+    }
+
+    public static void last_count(Integer last_count, Integer maxLast_count) {
+        // default: 0, no limit, use pn & ps
+        if (last_count < 0 || last_count > maxLast_count) {
+            throw new InvalidRequestParameterException("last_count", last_count, "last_count should between 0 and " + maxLast_count);
+        }
+    }
+
     public static void desc(Integer desc) {
         // default: 0 or 1, show desc order or not
         if (desc != 0 && desc != 1) {
