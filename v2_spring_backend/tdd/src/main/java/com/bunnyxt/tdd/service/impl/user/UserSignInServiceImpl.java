11c99f6d9fc9e351635030dfabd041c61c0286ac
bunnyxt <bunnyxt@outlook.com>
2020-02-27 14:08:53 +0800

    Use userSignInOverviewDao.

--- v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/service/impl/user/UserSignInServiceImpl.java
+++ v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/service/impl/user/UserSignInServiceImpl.java
@@ -1,24 +1,24 @@
 package com.bunnyxt.tdd.service.impl.user;
-import com.bunnyxt.tdd.dao.user.UserDao;
-import com.bunnyxt.tdd.dao.user.UserHistoryExpDao;
-import com.bunnyxt.tdd.dao.user.UserHistoryPointDao;
-import com.bunnyxt.tdd.dao.user.UserSignInDao;
+import com.bunnyxt.tdd.dao.user.*;
+import com.bunnyxt.tdd.model.TddCommonResponse;
 import com.bunnyxt.tdd.model.user.UserSignIn;
+import com.bunnyxt.tdd.model.user.UserSignInOverview;
 import com.bunnyxt.tdd.service.user.UserSignInService;
 import com.bunnyxt.tdd.util.CalendarUtil;
 import com.bunnyxt.tdd.util.PageNumModfier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class UserSignInServiceImpl implements UserSignInService {
 
     @Autowired
     UserSignInDao userSignInDao;
+
+    @Autowired
+    UserSignInOverviewDao userSignInOverviewDao;
 
     @Autowired
     UserDao userDao;
@@ -45,45 +45,38 @@ public class UserSignInServiceImpl implements UserSignInService {
     }
 
     @Override
-    public void postUserSignIn(Long userid) {
+    public TddCommonResponse postUserSignIn(Long userid) {
         // check today added or not
-        Integer start_ts = CalendarUtil.getTodayStartTs();
-        Integer end_ts = start_ts + 24 * 60 * 60 - 1;
+        Integer start_ts = CalendarUtil.getTodayStartTs(); // today start ts
+        Integer end_ts = start_ts + 24 * 60 * 60 - 1; // today end ts
         UserSignIn todayUserSignIn = userSignInDao.queryUserSignInOneDay(userid, start_ts, end_ts);
         if (todayUserSignIn != null) {
-            System.out.println("already added");
-            return;
+            return new TddCommonResponse("fail", "user have already signed in today");
         }
 
         // go sign in
         Integer added = CalendarUtil.getNowTs();
         userSignInDao.addUserSignIn(added, userid);
 
-        // check last 5 sign in
-        List<UserSignIn> last5UserSignIn = userSignInDao.queryUserSignIns(userid, 5, 0, 0, 0, 0);
-        int consecutive_sign_in_count = 0;
-        int start_ts_t = start_ts;
-        int end_ts_t = end_ts;
-        for (int i = 0; i < last5UserSignIn.size(); i++) {
-            int added_t = last5UserSignIn.get(i).getAdded();
-            if (added_t >= start_ts_t && added_t <= end_ts_t) {
-                consecutive_sign_in_count++;
-                start_ts_t -= 24 * 60 * 60;
-                end_ts_t -= 24 * 60 * 60;
-            } else {
-                break;
-            }
+        // get overview and check, then update overview
+        UserSignInOverview userSignInOverview = userSignInOverviewDao.queryUserSignInOverviewByUserid(userid);
+        Integer last_added = userSignInOverview.getLast_added();
+        Integer new_last_added_days = 1;
+        if (last_added >= start_ts - 24 * 60 * 60 && last_added <= end_ts - 24 * 60 * 60) {
+            // yesterday signed in, last_added_days++
+            new_last_added_days = userSignInOverview.getLast_added_days() + 1;
         }
+        userSignInOverviewDao.updateUserSignInOverviewByUseridWhenSignIn(userid, added, new_last_added_days);
 
         // add point
         double[] addPointArr = {0, 5, 10, 15, 20, 25};
-        double addPoint = addPointArr[consecutive_sign_in_count];
+        double addPoint = addPointArr[new_last_added_days > 5 ? 5 : new_last_added_days];
         userDao.updateUserPointById(addPoint, userid);
         userHistoryPointDao.addUserHistoryPoint(added, userid, addPoint, "每日登录奖励");
 
         // add exp
         double[] addExpArr = {0, 5, 10, 15, 20, 25};
-        double addExp = addExpArr[consecutive_sign_in_count];
+        double addExp = addExpArr[new_last_added_days > 5 ? 5 : new_last_added_days];
         userDao.updateUserExpById(addExp, userid);
         userHistoryExpDao.addUserHistoryExp(added, userid, addExp, "每日登录奖励");
 
@@ -91,6 +84,12 @@ public class UserSignInServiceImpl implements UserSignInService {
         int rank = userSignInDao.queryUserSignInRankInOneDay(userid, start_ts, end_ts);
 
         // return
-        System.out.println(rank);
+        Map<String, Object> detail = new LinkedHashMap<>();
+        detail.put("total", userSignInOverview.getTotal() + 1); // total signed in days
+        detail.put("consecutive_days", new_last_added_days); // consecutive signed in days
+        detail.put("point", addPoint); // earned point
+        detail.put("exp", addExp); // earned exp
+        detail.put("rank", rank); // today signed in rank
+        return new TddCommonResponse("success", "sign in success", detail);
     }
 }
