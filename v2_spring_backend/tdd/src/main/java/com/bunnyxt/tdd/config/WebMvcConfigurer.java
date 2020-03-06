90aa7552ce8df1a7c562e5ddb3529d397310e553
bunnyxt <bunnyxt@outlook.com>
2020-03-06 17:50:03 +0800

    Add WebMvcConfigurer.

---
+++ v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/config/WebMvcConfigurer.java
@@ -0,0 +1,22 @@
+package com.bunnyxt.tdd.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", // for debug
+                        "chrome-extension://phdhijbcfjgnhdajagackoddmfegcfeh", // for chrome extension
+                        "http://tdd.bunnyxt.com", "https://tdd.bunnyxt.com", // for production
+                        "http://tdd2.bunnyxt.com", "https://tdd2.bunnyxt.com") // for inner test
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Access-Control-Allow-Origin",
+                        "Access-Control-Allow-Headers", "Authorization", "X-Requested-With")
+                .allowCredentials(true);
+    }
+}
