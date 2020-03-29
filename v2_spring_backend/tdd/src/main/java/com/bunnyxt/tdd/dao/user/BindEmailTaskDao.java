b7ad473b6e0032450a4154425d5c9dce3938a602
bunnyxt <bunnyxt@outlook.com>
2020-03-29 23:15:39 +0800

    Add BindEmailTask model dao and mapper.

---
+++ v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/dao/user/BindEmailTaskDao.java
@@ -0,0 +1,13 @@
+package com.bunnyxt.tdd.dao.user;
+
+import com.bunnyxt.tdd.model.user.BindEmailTask;
+
+public interface BindEmailTaskDao {
+
+    void addBindEmailTask(Integer added, Long userid, String email, String bindkey, String code,
+                          Integer expired, Byte status);
+
+    BindEmailTask queryBindEmailTaskByBindKey(String bindkey);
+
+    void updateBindEmailTaskStatusByBindKey(String bindkey, Byte status);
+}
