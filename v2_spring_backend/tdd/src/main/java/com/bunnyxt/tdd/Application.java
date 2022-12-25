cb13e555802ff31cb6486e5842ac32df8407ec4f
Zhongyi-Lu <zhongyi_lu@outlook.com>
2022-12-24 23:45:01 -0800

    feat: use local environments

--- v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/Application.java
+++ v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/Application.java
@@ -3,9 +3,11 @@ package com.bunnyxt.tdd;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @MapperScan("com.bunnyxt.tdd.dao")
+@PropertySource("application-local.properties")
 public class Application {
 
     public static void main(String[] args) {
