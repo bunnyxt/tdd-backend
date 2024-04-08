e4e3277f511ea6742dca7d12a97324f9337d0ca6
yzqzss <30341059+yzqzss@users.noreply.github.com>
2024-04-08 19:06:08 +0000

    fix: av2bv

--- v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/util/TddAbidUtil.java
+++ v2_spring_backend/tdd/src/main/java/com/bunnyxt/tdd/util/TddAbidUtil.java
@@ -1,45 +1,56 @@
 package com.bunnyxt.tdd.util;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.List;
 
 public class TddAbidUtil {
 
-    private static String table = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF";
-    private static int[] s = {11, 10, 3, 8, 4, 6};
-    private static Long xor = 177451812L;
-    private static Long add = 8728348608L;
-    private static Map<Character, Integer> tr;
+    static long XOR_CODE = 23442827791579L;
+    static long MASK_CODE = 2251799813685247L;
+    static long MAX_AID = 1L << 51;
 
-    private static void initTr() {
-        tr = new HashMap<>();
-        for (int i = 0; i < 58; i++) {
-            tr.put(table.charAt(i), i);
+    static String ALPHABET = "FcwAPNKTMug3GV5Lj7EJnHpWsx4tb8haYeviqBz6rkCy12mUSDQX9RdoZf";
+
+    static List<Integer> ENCODE_MAP = Arrays.asList(8, 7, 0, 5, 1, 3, 2, 4, 6);
+    static List<Integer> DECODE_MAP = Arrays.asList(6, 4, 2, 3, 1, 5, 0, 7, 8);
+
+    static int BASE = ALPHABET.length();
+    static String PREFIX = "BV1";
+    static int PREFIX_LEN = PREFIX.length();
+    static int CODE_LEN = ENCODE_MAP.size();
+
+    public static String a2b(long aid) {
+        char[] bvid = new char[9];
+        long tmp = (MAX_AID | aid) ^ XOR_CODE;
+        for (int i = 0; i < CODE_LEN; i++) {
+            bvid[ENCODE_MAP.get(i)] = ALPHABET.charAt((int) (tmp % BASE));
+            tmp /= BASE;
         }
+        return (PREFIX + new String(bvid)).substring(2);  // remove BV prefix
     }
 
-    public static String a2b(Integer aid) {
-        Long x = Long.valueOf(aid);
-        x = (x ^ xor) + add;
-        char[] r = {'B', 'V', '1', ' ', ' ', '4', ' ', '1', ' ', '7', ' ', ' '};
-        for (int i = 0; i < 6; i++) {
-            r[s[i]] = table.charAt((int)(x / new Double(Math.pow(58, i)).longValue() % 58));
+
+    // no BV prefix provided
+    public static long b2a(String bvid) {
+        bvid = "BV" + bvid;
+        if (!bvid.substring(0, 3).equals(PREFIX)) {
+            throw new IllegalArgumentException("Invalid prefix in bvid: " + bvid);
         }
-        StringBuilder stringBuilder = new StringBuilder();
-        for (char rr: r) {
-            stringBuilder.append(rr);
+
+        long tmp = 0;
+        bvid = bvid.substring(3);
+        for (int i = 0; i < CODE_LEN; i++) {
+            int idx = ALPHABET.indexOf(bvid.charAt(DECODE_MAP.get(i)));
+            tmp = tmp * BASE + idx;
         }
-        return stringBuilder.toString().substring(2); // remove BV prefix
+        return (tmp & MASK_CODE) ^ XOR_CODE;
     }
 
-    public static Integer b2a(String bvid) {
-        if (tr == null) {
-            initTr();
-        }
-        Long r = 0L;
-        for (int i = 0; i < 6; i++) {
-            r += tr.get(bvid.charAt(s[i] - 2)) * new Double(Math.pow(58, i)).longValue(); // no BV prefix provided
-        }
-        return new Long((r - add) ^ xor).intValue();
+    public static void main(String[] args) {
+        long aid1 = 1152551018L;
+        String bvid1 = a2b(aid1);
+        System.out.println(bvid1); // Output: 1MZ421B7CB
+        long decodedAid1 = b2a(bvid1);
+        System.out.println(decodedAid1); // Output: 1152551018
     }
 }
